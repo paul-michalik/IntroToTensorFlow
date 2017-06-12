064517604120f0199c76ebcd583641b97232cfe4
PMichalik <paul.michalik@harman.com>
2017-06-12 19:47:09 +0200

    Functional env.bat

--- env.bat
+++ env.bat
@@ -1,8 +1,24 @@
 @echo off
+
 setlocal
-conda remove --name tensorflow --all
-conda create -n tensorflow
-activate tensorflow
-conda install python=3.5
+call deactivate.bat
+echo 1
+if %errorlevel% neq 0 echo Error occured! & exit /b 1
+
+conda remove -y --all --name tensorflow
+echo 2
+if %errorlevel% neq 0 echo Error occured! & exit /b 1
+
+conda create -y --name tensorflow python=3.5 anaconda
+echo 3
+if %errorlevel% neq 0 echo Error occured! & exit /b 1
+
+call activate.bat tensorflow
+echo 4
+if %errorlevel% neq 0 echo Error occured! & exit /b 1
+
 pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/windows/cpu/tensorflow-1.1.0-cp35-cp35m-win_amd64.whl
+echo 5
+if %errorlevel% neq 0 echo Error occured! & exit /b 1
+
 endlocal
